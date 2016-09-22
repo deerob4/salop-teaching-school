9629d52d79bbecd619abdffb587167d3e40bc452
Dee Roberts <deerob4@gmail.com>
2016-09-22 10:41:10 +0100

    Bump version point

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule TeachingSchool.Mixfile do
 
   def project do
     [app: :teaching_school,
-     version: "1.0.0",
+     version: "1.0.1",
      elixir: "~> 1.3",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
